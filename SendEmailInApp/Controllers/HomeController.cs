dc22b7542db441c12d289e90f05210877946caff
Jordan Rosas <jordanrosas1203@gmail.com>
2019-05-07 14:47:33 -0500

    working commit

--- SendEmailInApp/Controllers/HomeController.cs
+++ SendEmailInApp/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
 using SendEmailInApp.Models;
+//using SendEmailInAspNetCore.Helpers;
+//using SendEmailInAspNetCore.Models;
+using System;
+using System.Diagnostics;
 
 namespace SendEmailInApp.Controllers
 {
@@ -12,6 +14,51 @@ namespace SendEmailInApp.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel contactViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //instantiate a new MimeMessage
+                    var message = new MimeMessage();
+                    //Setting the To e-mail address
+                    message.To.Add(new MailboxAddress("Recipient Name", "Recipient Email"));
+                    //Setting the From e-mail address
+                    message.From.Add(new MailboxAddress("Sender Name", "Sender Email"));
+                    //E-mail subject 
+                    message.Subject = contactViewModel.Subject;
+                    //E-mail message body
+                    message.Body = new TextPart(TextFormat.Html)
+                    {
+                        Text = contactViewModel.Message + " Message was sent by: " + contactViewModel.Name + " E-mail: " + contactViewModel.Email
+                    };
+
+                    //Configure the e-mail
+                    using (var emailClient = new SmtpClient())
+                    {
+                        emailClient.Connect("smtp.gmail.com", 587, false);
+                        emailClient.Authenticate("Authenticate Sender Email", "Password");
+                        emailClient.Send(message);
+                        emailClient.Disconnect(true);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $" Oops! We have a problem here {ex.Message}";
+                }
+            }
             return View();
         }
 
